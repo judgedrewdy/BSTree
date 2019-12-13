b728c3ac575c9c9c7755b855c407f66c405295dc
judgedrewdy <55523482+judgedrewdy@users.noreply.github.com>
2019-12-12 18:04:06 -0600

    Add files via upload

--- BSTManager.java
+++ BSTManager.java
@@ -9,7 +9,7 @@ public class BSTManager {
 		System.out.println("\nWhat would you like to do to the list?");
 		System.out.println("Press 'i' to insert an integer to the tree.");
 		System.out.println("Press 'r' to remove an integer from the tree.");
-		System.out.println("Press 's' to find an integer in the tree.");
+		System.out.println("Press 'f' to find an integer in the tree.");
 		System.out.println("Press 'p' to print the tree in ascending order.");
 		System.out.println("Press 'd' to print the depth of the tree.");
 		System.out.println("Press 'q' to quit.");
@@ -21,8 +21,6 @@ public class BSTManager {
 		
 		Scanner scnr = new Scanner(System.in);
 		
-		
-		//printMenu();
 		String input = " ";
 		
 		while (!input.equals("q")) {
@@ -42,40 +40,49 @@ public class BSTManager {
 			else if (input.equals("p")) {
 				newTree.BSTPrintInorder(newTree.root);
 			}
-//			else if (input.equals("i")) {
-//				System.out.println("Type which integer you would like to insert after.");
-//				int insertAfter = scnr.nextInt();
-//				scnr.nextLine();
-//				System.out.println("Type the integer you would like to insert ");
-//				int inserted = scnr.nextInt();
-//				scnr.nextLine();
-//				newList.insertAfter(insertAfter, inserted);
-//			
-//			}
-//			else if (input.equals("r")) {
-//				System.out.println("Type the integer you would like to remove.");
-//				if (scnr.hasNextInt()) {
-//				int removed = scnr.nextInt();
-//				scnr.nextLine();
-//				newList.remove(removed);
-//				}
-//				else {
-//					System.out.println("Not an integer.");
-//				}
-//			}
-//			
-//			else if (input.equals("g")) {
-//				System.out.println(newList.listLength());
-//			}
-//			else if (input.equals("l")) {
-//				newList.printList();
-//			}
-//			else if (input.equals("q")) {
-//				break;
-//			}
-//			else {
-//				System.out.println("Not valid input.");
-//			}
+			else if (input.equals("r")) {
+				System.out.println("Type the integer you would like to remove.");
+				if (scnr.hasNextInt()) {
+					int remove = scnr.nextInt();
+					scnr.nextLine();
+					if(newTree.BSTRemove(remove)) {
+						System.out.println(remove + " has been removed from the tree.");
+					}
+					else {
+						System.out.println(remove + " not found in tree.");
+					}
+				}
+				else {
+					System.out.println("Not an integer.");
+				}
+			}
+			else if(input.contentEquals("f")) {
+				System.out.println("Type the integer you would like to find.");
+				if (scnr.hasNextInt()) {
+					int find = scnr.nextInt();
+					scnr.nextLine();
+					if(newTree.BSTFind(find)) {
+						System.out.println(find + " has been found in the tree.");
+					}
+					else {
+						System.out.println(find + " not found in tree.");
+					}
+				}
+				else {
+					System.out.println("Not an integer.");
+				}
+			}
+			else if (input.equals("d")) {
+				System.out.println("The maximum depth of the tree is: " + newTree.maxDepth(newTree.root));
+			
+			}
+			
+			else if (input.equals("q")) {
+				break;
+			}
+			else {
+				System.out.println("Not valid input.");
+			}
 //			
 		}
 	}
--- BinaryST.java
+++ BinaryST.java
@@ -43,15 +43,109 @@ public class BinaryST {
 		}
 	}
 	
+	public boolean BSTRemove(int key) {
+		boolean found = false;
+		Node par = new Node();
+		Node cur = new Node();
+		par = null;
+		cur = root;
+		while(cur != null && !found) {
+			if(cur.getData() == key) {
+				if (cur.getLeftChild() == null && cur.getRightChild() == null) {
+					if(par == null) {
+						root = null;
+					}
+					else if(par.getLeftChild().equals(cur)){
+						par.setLeftChild(null);
+					}
+					else {
+						par.setRightChild(null);
+					}
+				}
+				else if (cur.getLeftChild() != null && cur.getRightChild() == null) {
+					if (par == null) {
+						root = cur.getLeftChild();
+					}
+					else if (par.getLeftChild().equals(cur)) {
+						par.setLeftChild(cur.getLeftChild());
+					}
+					else {
+						par.setRightChild(cur.getLeftChild());
+					}
+				}
+				else if (cur.getLeftChild() == null && cur.getRightChild() != null) {
+					if(par == null) {
+						root = cur.getRightChild();
+					}
+					else if(par.getLeftChild().equals(cur)) {
+						par.setLeftChild(cur.getRightChild());
+					}
+					else {
+						par.setRightChild(cur.getRightChild());
+					}
+				}
+				else {
+					Node suc = new Node();
+					suc = cur.getRightChild();
+					while(suc.getLeftChild() != null) {
+						suc = suc.getLeftChild();
+					}
+					int successorData = suc.getData();
+					BSTRemove(suc.getData());
+					cur.setData(successorData);
+				}
+				found = true;
+			}
+			else if(cur.getData() < key) {
+				par = cur;
+				cur = cur.getRightChild();
+			}
+			else {
+				par = cur;
+				cur = cur.getLeftChild();
+			}
+		}
+		
+		return found;
+	}
 	public void BSTPrintInorder(Node root) {  
 		Node node = new Node();
 		node = root;
 		if (node == null) {
 		      return;                    
-		  }
+		}
 		  
-		  BSTPrintInorder(node.getLeftChild());   
-		  System.out.print(node.getData() + " ");                    
-		  BSTPrintInorder(node.getRightChild()) ; 
+		BSTPrintInorder(node.getLeftChild());   
+		System.out.print(node.getData() + " ");                    
+		BSTPrintInorder(node.getRightChild()) ; 
+	}
+	public boolean BSTFind(int key) {  
+		return BSTFindRecursive(root, key);
+	}
+	
+	private boolean BSTFindRecursive(Node node, int key) {
+		boolean found = false;
+		if (node != null) {
+			if (key == node.getData()) {
+				found = true;
+			}
+			else if (key < node.getData()) {
+				return BSTFindRecursive(node.getLeftChild(), key);
+			}
+			else {
+				return BSTFindRecursive(node.getRightChild(), key);
+			}
+		}
+		return found;
+	}
+	public int maxDepth(Node node) {
+		   if (node == null) {
+			   return 0;
+		   }
+		   else {
+			   int leftDepth = maxDepth(node.getLeftChild());
+			   int rightDepth = maxDepth(node.getRightChild());
+			   return Math.max(leftDepth, rightDepth) + 1;
+		   }
 		}
 }
